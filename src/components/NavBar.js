619f530c779b4a29c5329143b4046fc4316fde0d
BeckySkel <beckyskelcher@hotmail.co.uk>
2023-03-06 14:00:40 +0000

    Refactor NavBar.js and add notes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,66 +1,63 @@
-import { React, useContext, useState } from 'react';
-import PatchStyles from 'patch-styles';
-import styles from '../styles/NavBar.module.css';
-import { Container, Navbar, Nav, Fade } from 'react-bootstrap';
+// External imports
+import { React, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import PatchStyles from 'patch-styles';
+import { Container, Navbar, Nav, Fade } from 'react-bootstrap';
+import slugify from 'react-slugify';
+// Internal imports
 import { useCurrentUser } from '../contexts/CurrentUserContext';
+import styles from '../styles/NavBar.module.css';
+import appStyles from '../App.module.css';
 
 
+/* 
+Simple navbar which displays the logo (links to homepage) in the far left
+and a dynamic (different links if user logged in or out) account menu on the right 
+*/
 const NavBar = () => {
-  const [open, setOpen] = useState(false);
-
+  const [accountMenuOpen, setAccountMenuOpen] = useState(false);
   const currentUser = useCurrentUser();
-  const loggedInAccountMenu = (
-    <>
-      <Nav.Item>
-        <NavLink to="/logout" className="nav-link">Logout</NavLink>
-      </Nav.Item>
-      <Nav.Item>
-        <NavLink to="/edit-profile" className="nav-link">Edit Profile</NavLink>
-      </Nav.Item>
-    </>
-  );
-  const loggedOutAccountMenu = (
-    <>
-      <Nav.Item>
-        <NavLink to="/login" className="nav-link">Login</NavLink>
-      </Nav.Item>
-      <Nav.Item>
-        <NavLink to="/register" className="nav-link">Register</NavLink>
-      </Nav.Item>
-    </>
-  );
+  const Links = currentUser ? ['Logout', 'Edit Profile'] : ['Login', 'Register'];
 
   return (
     <PatchStyles classNames={styles}>
+      <PatchStyles classNames={appStyles}>
 
-      <Navbar className="bg-gradient NavBar" variant="dark">
+      <Navbar className="bg-gradient Bg-Navy NavBar" variant="dark">
         <Container fluid>
+          {/* Logo (links to homepage) */}
           <NavLink to="/">
-            <Navbar.Brand className="ps-2">
+            <Navbar.Brand className="ps-1">
               <div className="circle d-inline-block align-top"></div>
               <span className="logo">Devise</span>
             </Navbar.Brand>
           </NavLink>
 
+          {/* Stylised account menu which expands when hovered over */}
           <div className="account-menu-container"
-            onMouseEnter={() => setOpen(!open)}
-            onMouseLeave={() => setOpen(!open)}
+            onMouseEnter={() => setAccountMenuOpen(!accountMenuOpen)}
+            onMouseLeave={() => setAccountMenuOpen(!accountMenuOpen)}
             aria-controls="account-menu"
-            aria-expanded={open}>
-            <i className="fas fa-user user-icon"></i>
+            aria-expanded={accountMenuOpen}>
+              {/* User icon */}
+            <i aria-label="Account Menu" className="fas fa-user user-icon"></i>
 
-            <Fade in={open} id="account-menu" className="account-menu">
+            {/* Links rendered as list */}
+            <Fade in={accountMenuOpen} id="account-menu" className="account-menu">
               <Nav className="flex-column">
-                { currentUser ? loggedInAccountMenu : loggedOutAccountMenu }
+                  {Links.map(link => (
+                    <Nav.Item key={link}>
+                      <NavLink to={`/${slugify(link)}`} className="nav-link">{link}</NavLink>
+                    </Nav.Item>
+                  ))}
               </Nav>
             </Fade>
           </div>
-
         </Container>
       </Navbar>
 
     </PatchStyles >
+    </PatchStyles>
   )
 }
 
